0ecf1ee0b833a655b62018c06ed6b5d07649faec
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-05-30 03:41:07 +0900

    while 문 사용 (C#)
    -

--- Program.cs
+++ Program.cs
@@ -1,130 +1,40 @@
-﻿using System.Collections;
-using System.Linq.Expressions;
-using System.Reflection.Metadata.Ecma335;
+﻿using System;
 
-namespace Csharp_practice
+namespace Csharp
+
 {
-
-    enum Choice_symbol {
-        Rock = 1,
-        Scissors = 0,
-        Paper=2 
-    }
     class Program
     {
+       
         static void Main(string[] args)
         {
-         
-            // 0 : 가위 , 1 : 바위 , 2 : 보 
+            int num;
+            bool isPrime = true;
 
-            Random rand = new Random();
-            int aichoice = rand.Next(0, 3); // aichoice 변수에 난수 생성 0~2 까지
+            Console.WriteLine("소수인지 판별 할 숫자를 입력하시오 : ");
+            num = Convert.ToInt32(Console.ReadLine());
 
+            //Console.WriteLine(num);
 
-            Console.WriteLine(" 0 : 가위 , 1 : 바위, 2 : 보 입니다." );
-            Console.WriteLine("=================================");
-            Console.WriteLine("=================================");
-
-            Console.WriteLine("내고싶은 것을 숫자로 입력하세요 :  ");
-            int choice = Convert.ToInt32(Console.ReadLine()); //사용자가 낼 것을 숫자로 입력받음
-
-
-
-            switch (choice)
+            for(int i = 2; i<num; i++)
             {
 
-                case (int)Choice_symbol.Scissors:
-                    Console.WriteLine("당신의 선택은 가위 입니다.");
+                if(num%i ==0)
+                    isPrime = false;
+                break;
 
-                    break;
-                case (int)Choice_symbol.Rock:
-                    Console.WriteLine("당신의 선택은 바위 입니다.");
-                    break;
-                case (int)Choice_symbol.Paper:
-                    Console.WriteLine("당신의 선택은 보 입니다.");
-                    break;
-            }
-
-
-            Console.WriteLine("=================================");
-            Console.WriteLine("=================================");
-            
-
-
-            switch (aichoice)
-            {
-
-                case (int)Choice_symbol.Scissors:
-                    Console.WriteLine("컴퓨터의 선택은 가위 입니다.");
-
-                    break;
-                case (int)Choice_symbol.Rock:
-                    Console.WriteLine("컴퓨터의 선택은 바위 입니다.");
-                    break;
-                case (int)Choice_symbol.Paper:
-                    Console.WriteLine("컴퓨터의 선택은 보 입니다.");
-                    break;
+              
+             
 
             }
-            Console.WriteLine("=================================");
-            Console.WriteLine("=================================");
-
-            //컴퓨터의 난수와 사용자의 지정 수를 통해 최종 비교연산을 통한 승부 
-
-            if (choice == (int)Choice_symbol.Scissors)
-            {
-                bool Draw = (aichoice == choice) ? true : false;
-
-                if (Draw)
-                    Console.WriteLine("비겼습니다.");
-                else if (aichoice == (int)Choice_symbol.Rock)
-                    Console.WriteLine("당신이 졌습니다.");
-                else
-                    Console.WriteLine("당신이 이겼습니다 ! ");
-
-            }
-
-            else if (choice == (int)Choice_symbol.Rock)
-            {
-                bool Draw = (aichoice == choice) ? true : false;
-
-                if (Draw)
-                    Console.WriteLine("비겼습니다.");
-                else if (aichoice == (int)Choice_symbol.Scissors)
-                    Console.WriteLine("당신이 이겼습니다 !");
-                else
-                    Console.WriteLine("당신이 졌습니다. ");
-
-            }
-
+            if (isPrime)
+                Console.WriteLine("소수 입니다.!");
             else
-            {
-
-                bool Draw = (aichoice == choice) ? true : false;
-
-                if (Draw)
-                    Console.WriteLine("비겼습니다.");
-                else if (aichoice == (int)Choice_symbol.Scissors)
-                    Console.WriteLine("당신이 졌습니다.");
-                else
-                    Console.WriteLine("당신이 이겼습니다. ");
-
-
-
-            }
-
-
-
-        }
-
-
-
-
-
-
+                Console.WriteLine("소수가 아닙니다!");
 
 
 
 
         }
     }
+}
