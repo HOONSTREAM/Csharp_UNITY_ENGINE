7985a17453c8f1679a96a6442388d82288467ef4
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-05-30 20:08:21 +0900

    -
    -

---
+++ out 키워드 예제.cs
@@ -0,0 +1,26 @@
+﻿namespace outs {
+internal class Program
+{
+
+    static void Divide(int a, int b, out int result1, out int result2)
+    {
+        result1 = a % b;
+        result2 = a / b;
+
+    }
+    static void Main(string[] args)
+    {
+        int num1 = 10;
+        int num2 = 3;
+        int re1;
+        int re2;
+
+        Divide(num1, num2, out re1, out re2);
+
+        Console.WriteLine(re1);
+        Console.WriteLine(re2);
+
+    }
+}
+}
+
