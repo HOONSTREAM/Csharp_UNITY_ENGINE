330c75601ab10202e49f9ac1d39d855b298caa26
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-05-31 14:25:17 +0900

    인프런 연습문제 1강 기초
    2중 for문 활용 구구단 출력, 별 쌓기, 팩토리얼 함수 구현

---
+++ 연습문제 2.cs
@@ -0,0 +1,26 @@
+﻿namespace 연습문제_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           // 별세우기 연습문제 
+
+
+
+
+            for(int i = 0; i<5; i++)
+            {
+
+                for(int j = 0; j<=i; j++)
+                {
+                    Console.Write("*");
+                    
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ 연습문제 3.cs
@@ -0,0 +1,29 @@
+﻿namespace 연습문제_3
+{
+    internal class Program
+    {
+
+        static int Factorial(int n)
+        {
+            int refs = 1;
+           
+            for(int num=1; num<=n; num++)
+            {
+                refs = (refs * num);
+
+            }
+
+            return refs;
+        }
+        static void Main(string[] args)
+        {
+
+            int n;
+
+            n = Program.Factorial(5);
+
+            Console.WriteLine(n);
+
+        }
+    }
+}
