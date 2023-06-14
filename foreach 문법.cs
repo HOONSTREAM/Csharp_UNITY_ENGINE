ce86478bf721cf6181526aaad4369a979c18f2b6
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-06-15 00:48:20 +0900

    foreach 문법 (C#)
    -

---
+++ foreach 문법.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] b = new int[5];
+
+            b[0] = 10;
+            b[1] = 20;
+            b[2] = 30;
+            b[3] = 40;
+            b[4] = 50;
+
+            foreach(int bs in b)
+            {
+                Console.WriteLine(bs);
+            }
+        }
+    }
+}
