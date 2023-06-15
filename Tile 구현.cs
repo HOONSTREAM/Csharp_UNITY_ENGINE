86c7bc33831b2ecc0cb7cf45297aa1133a57186a
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-06-16 03:12:05 +0900

    4방향 타일 구현 연습
    -

---
+++ Tile 구현.cs
@@ -0,0 +1,49 @@
+﻿namespace r
+{
+    internal class Program
+    {
+
+        class Map
+        {
+            int[,] map =
+            {
+                {1,1,1,1,1 },
+                {1,0,0,0,1 },
+                {1,0,0,0,1 },
+                {1,0,0,0,1 },
+                {1,1,1,1,1 }
+
+            };
+
+            public void Rander()
+            {
+                var Color = Console.ForegroundColor;
+
+                for(int y = 0; y < map.GetLength(0); y++)
+                {
+                    for(int x = 0; x < map.GetLength(1); x++)
+                    {
+                        if (map[y, x] == 1)
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Green;
+
+                        Console.Write("\u25cf");
+
+                    }
+                   Console.WriteLine();
+
+                }
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Map map = new Map();
+
+            map.Rander();
+
+
+        }
+    }
+}
