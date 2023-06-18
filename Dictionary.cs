ee561a466f6a4852ebf92575495d1b8443a0f88c
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-06-19 00:06:48 +0900

    Dictionary 기초

---
+++ Dictionary.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Specialized;
+
+namespace Dictionary
+{
+
+    class Monster
+    {
+        private int id;
+        public Monster (int id)
+        {
+            this.id = id;
+
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Monster mon = null;
+            Dictionary<int,Monster> dic = new Dictionary<int,Monster>();
+
+            for(int i = 0; i < 10000; i++)
+            {
+                dic.Add(i, new Monster(i));
+
+            }
+
+           bool found =  dic.TryGetValue(666, out mon);
+
+            Console.WriteLine(found);
+
+        }
+    }
+}
