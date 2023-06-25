0ae03796368c5731518bd86e250b8245f2b27da5
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-06-25 13:02:58 +0900

    Event 예제
    -

---
+++ Event (이벤트) 델리게이트 랩핑/Event (이벤트) 델리게이트 랩핑/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event__이벤트__델리게이트_랩핑
+{
+    //옵저버패턴
+    class InputManager
+    {
+
+        public delegate void OninputKey();
+        public event OninputKey inputkey; 
+
+        public void Update()
+        {
+            if (Console.KeyAvailable == false)
+                return;
+
+            ConsoleKeyInfo info = Console.ReadKey();
+            if(info.Key == ConsoleKey.A)
+            {
+                //모두에게 알려준다!
+                inputkey();
+
+            }
+
+
+            
+
+            
+        }
+    }
+}
---
+++ Event (이벤트) 델리게이트 랩핑/Event (이벤트) 델리게이트 랩핑/program.cs
@@ -0,0 +1,25 @@
+﻿namespace Event__이벤트__델리게이트_랩핑
+{
+    internal class Program
+    {
+        static void OninputTest()
+        {
+
+            Console.WriteLine("input received 구독완료");
+        }
+        static void Main(string[] args)
+        {
+            InputManager inputmanager = new InputManager();
+
+            inputmanager.inputkey += OninputTest;
+
+            while (true){
+
+                inputmanager.Update();
+
+            }
+
+
+        }
+    }
+}
