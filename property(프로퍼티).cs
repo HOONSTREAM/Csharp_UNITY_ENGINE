4a2bdc54cce4bc39b4bb1661b5fbae92fb3b47af
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-06-25 10:15:27 +0900

    프로퍼티 문법
    -

---
+++ property(프로퍼티).cs
@@ -0,0 +1,39 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Property
+{
+
+    // property : 프로퍼티 , (get,set 함수를 한군데에다 묶는다.)
+    internal class Program
+    {
+        class Knight
+        {
+            public int Hp { get; set; } = 100; // 프로퍼티
+        }
+
+        class Archor
+        {
+            public int mp //프로퍼티
+            {
+                get { return mp; }
+                set { mp = value; }
+            }
+            static void Main(string[] args)
+            {
+
+
+                Knight knight = new Knight();
+
+                Console.WriteLine(knight.Hp);
+
+                Archor archor = new Archor();
+                archor.mp=100;
+                int Mpvalue = archor.mp;
+                Console.WriteLine(Mpvalue);
+
+
+
+            }
+        }
+    }
+}
