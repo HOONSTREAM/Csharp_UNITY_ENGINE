d68af3d1f79915b412362a219ceabdf2ec71259d
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-06-25 10:51:35 +0900

    Delegate 기초개념
    -

---
+++ delegate(대리자).cs
@@ -0,0 +1,38 @@
+﻿namespace 대리자_delegate_
+{
+    internal class Program
+    {
+        delegate int Oncliked();
+
+        static int TestDelegate()
+        {
+            Console.WriteLine("Hello delegate");
+
+            return 0;
+        }
+
+        static int TestDelegate2()
+        {
+
+            Console.WriteLine("Hello Delegate2");
+
+            return 0;
+        }
+
+        static void ButtonPressed(Oncliked clikedFunction)
+        {
+            clikedFunction();
+        }
+        static void Main(string[] args)
+        {
+            Oncliked clicked = new Oncliked(TestDelegate);
+
+            clicked += TestDelegate2;
+
+            ButtonPressed(clicked);
+
+
+  
+        }
+    }
+}
