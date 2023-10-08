4e23b2fd16a57561ab1253ff04b4496ab23f0944
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2023-10-08 22:41:17 +0900

    Delete 23.09.30 MMORPG FrameWork 2 directory

--- 23.09.30 MMORPG FrameWork 2/UI/SubItem/ItemEffect.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ItemEffect : ScriptableObject //추상클래스
-{
-    public abstract bool ExecuteRole();
-}
